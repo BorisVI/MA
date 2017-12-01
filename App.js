f8b0e5a8fcb26bf01727d452236d05572604b90d
jordy <jordy.vandenbrande@student.ucll.be>
2017-12-01 11:34:49 +0100

    ff fixje voor u <3

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as Test from './test';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open {Test.foo()} up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
