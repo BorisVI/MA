8ca644d44b2bb581036688fe235d18b5e2b322ac
Kevin <kevin.peelman@student.ucll.be>
2017-12-09 18:57:21 +0100

    added json etc to package.lock, got map and json to work

--- domain/currency.ts
+++ domain/currency.ts
@@ -1,8 +1,34 @@
-import {map} from "typescript-map"
+import { TSMap } from "../node_modules/typescript-map";
+import * as express from "express";
+import {Server, Path, GET, PathParam} from "typescript-rest";
 class Currency{
     private _name : string;
-    public rateToCurrencies : map;
-    constructor(){
-        
+    private _rateToCurrencies : TSMap<string,number>;
+    constructor(name: string){
+        this._name = name;
+        this.update();
     }
+
+    update(){
+        const json = 'https://api.fixer.io/latest?base='+ this._name;
+        let jsonobj = JSON.parse(json);
+        for(let entry in jsonobj.rates){
+            console.log(entry);
+        }
+    }
+
+    set name(name:string){
+        this._name = name;
+     }
+     set rateToCurrecies(rateToCurrecies:TSMap<string,number>){
+         this._rateToCurrencies = rateToCurrecies;
+     }
+     
+     get name(): string{
+         return this._name;
+     }
+     get rateToCurrencies() : TSMap<string,number>{
+         return this._rateToCurrencies;
+     }
+
 }
