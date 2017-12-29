9c064fc91c2173c1cbf11b2c402d330c507de274
Boris Van Ingelgom <Boris@10.25.124.5>
2017-12-29 12:32:58 +0100

    enumfix

--- domain/category.js
+++ domain/category.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Category;
 (function (Category) {
-    Category[Category["OvernightStay"] = 0] = "OvernightStay";
-    Category[Category["Transport"] = 1] = "Transport";
-    Category[Category["Activity"] = 2] = "Activity";
-    Category[Category["Food"] = 3] = "Food";
-    Category[Category["Misc"] = 4] = "Misc";
+    Category["OvernightStay"] = "OvernightStay";
+    Category["Transport"] = "Transport";
+    Category["Activity"] = "Activity";
+    Category["Food"] = "Food";
+    Category["Misc"] = "Misc";
 })(Category = exports.Category || (exports.Category = {}));
